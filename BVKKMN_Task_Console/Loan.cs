b4815586717a6278f7b245c8927385b989fd5f73
Greta Skipare <gretchfurlined@gmail.com>
2021-12-29 14:55:48 +0200

    Initial commit

---
+++ BVKKMN_Task_Console/Loan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BVKKMN_Task_Console
+{
+    class Loan
+    {
+        private double LoanAmount, InterestRate;
+        private int LoanLength, LoanPayDay;
+
+        public Loan(double amount, int term, double rate, int payday)
+        {
+            this.LoanAmount = amount;
+            this.LoanLength = term;
+            this.InterestRate = rate;
+            this.LoanPayDay = payday;
+        }
+
+        public double GetMonthlyPayment()
+        {
+            return (LoanAmount * InterestRate / 1200 * Math.Pow(1 + InterestRate, LoanLength)) / (Math.Pow(1 + InterestRate, LoanLength) - 1);
+        }
+
+        public void LoanTable()
+        {
+            double monthlyPayment = GetMonthlyPayment();
+            double principalPaid = 0;
+            double newBalance = 0;
+            double interestPaid = 0;
+            double principal = LoanAmount;
+            double totalinterest = 0;
+            double loanLeft = 0;
+
+            Console.WriteLine("{0,10}{1,10}{2,10}{3,10}{4,10}", "Payment No.", "Part of the Loan", "Interest Paid", "Payment", "Balance");
+            for (int month = 1; month <= LoanLength; month++)
+            {
+                totalinterest += interestPaid;
+                interestPaid = principal * InterestRate;
+                principalPaid = monthlyPayment - interestPaid;
+                newBalance = principal - principalPaid;
+                loanLeft = newBalance - monthlyPayment;
+
+
+                Console.WriteLine("{0,-10}{1,10}{2,10}{3,10}{4,10}",
+                    month, loanLeft, interestPaid, monthlyPayment, newBalance);
+
+                principal = newBalance;
+            }
+        }
+    }
+}
---
+++ BVKKMN_Task_Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BVKKMN_Task_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount;
+            int term;
+            double rate;
+            int payday;
+
+            Console.WriteLine("BVKKMN Console Calculator App");
+            Console.WriteLine("-----------------------------");
+
+            Console.WriteLine("Type Loan Amount and press Enter");
+            amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Type Term in Months and press Enter");
+            term = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Type Annual Interest Rate % and press Enter");
+            rate = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Type Payment Date (1-31) and press Enter");
+            payday = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("-----------------------------");
+
+            Loan loan = new Loan(amount, term, rate, payday);
+
+            loan.LoanTable();
+
+            Console.ReadKey();
+        }
+    }
+}
